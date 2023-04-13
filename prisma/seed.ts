1ac2969368cb72484ce522f3b0c23bfbd2a03def
jack <43416057+member87@users.noreply.github.com>
2023-04-13 22:42:51 +0100

    Removed comments

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -44,89 +44,10 @@ function getRandomInt(max: number) {
   return Math.floor(Math.random() * max);
 }
 
-/*
-async function addLocations() {
-  const locations: Location[] = [];
-  for (let i = 0; i < 5; i++) {
-    const v: string = getRandom(venues);
-    const e = await prisma.location.upsert({
-      where: { venue: v },
-      update: {},
-      create: {
-        venue: v,
-        lat: 0,
-        long: 0,
-        city: "London",
-        country: "United Kingdom"
-      }
-    })
-    locations.push(e);
-  }
-
-  return locations;
-}
-
-async function addEvents() {
-  const locations: Location[] = await prisma.location.findMany();
-  const eventList: Event[] = [];
-  for (let i = 0; i < 25; i++) {
-    const event: string = getRandom(events);
-    const location: Location = getRandom(locations)
-
-    const e = await prisma.event.upsert({
-      where: {
-        event_identifier: {
-          name: event,
-          locationId: location.id
-        }
-      },
-      update: {},
-      create: {
-        name: event,
-        locationId: location.id,
-        time: 0,
-      }
-    })
-
-
-    eventList.push(e);
-  }
-
-  return eventList;
-}
-
-async function addTickets() {
-  const eventList: Event[] = await prisma.event.findMany()
-  eventList.forEach(async (e: Event) => {
-    for (let i = 0; i < getRandomInt(100); i++) {
-      const seat = `A${i}`;
-      await prisma.ticket.upsert({
-        where: {
-          ticket_identifier: {
-            eventId: e.id,
-            seat: seat,
-          }
-        },
-        update: {},
-        create: {
-          eventId: e.id,
-          seat: seat,
-          price: 100,
-
-        }
-      })
-    }
-  })
-}
-*/
-
 async function main() {
-  /*
-  await addLocations();
-  await addEvents();
-  await addTickets();
-  */
-
+  /**
+   * Create test music genre
+   */
   await prisma.genre.upsert({
     where: {
       genre: "TEST"
@@ -138,9 +59,10 @@ async function main() {
   })
 
 
+  /**
+   * Add venues into database
+   */
   for (const city in venues) {
-    console.log(city, venues[city])
-
     venues[city].forEach(async (venue: string) => {
       const venueRecord = await prisma.location.upsert({
         where: {
@@ -155,16 +77,14 @@ async function main() {
           long: 0,
         }
       })
-
-
     })
-
   }
 
 
-
+  /**
+   * Add artists into database
+   */
   for (const artist in artists) {
-    const events: string[] = artists[artist]
     const artistRecord = await prisma.artist.upsert({
       where: {
         name: artist,
@@ -183,8 +103,6 @@ async function main() {
     artists[artist].forEach(async (eventName: string) => {
       for (let i = 0; i < getRandomInt(5) + 1; i++) {
         const location = getRandom(await prisma.location.findMany())
-
-        console.log(i, location.id, eventName, artist)
 
         await prisma.event.upsert({
           where: {
