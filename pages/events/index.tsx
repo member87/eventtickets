fa112ea5a2a8a224a291161bda22c357ae189f8e
jack <43416057+member87@users.noreply.github.com>
2023-04-15 02:25:09 +0100

    Added event list page

---
+++ pages/events/index.tsx
@@ -0,0 +1,11 @@
+import { Page } from '@/components/templates/page';
+import { Events } from '@/components/loop/events/events';
+
+
+export default function EventsPage() {
+  return (
+    <Page>
+      <Events />
+    </Page>
+  )
+}
